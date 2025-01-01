e567dd08ccfb5bf4492cd6720a1344838556722d
Sergio Perea <seperu.7@gmail.com>
2025-01-01 20:50:08 +0100

    Updates

--- tienda-react/src/models/producto.ts
+++
@@ -1,8 +0,0 @@
-export interface ProductoInterface {
-  id: number
-  title: string
-  price: number
-  description: string
-  category: string
-  image: string
-}
--- tienda-react/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/js_proyects/React-shop/',
-  plugins: [react()],
-})
