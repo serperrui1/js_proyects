3dd1c600895ef921ef6dec64516895443b21416e
Sergio Perea <seperu.7@gmail.com>
2025-01-01 21:05:01 +0100

    Actualización de GitHub Pages

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Limpiar la rama gh-pages (si existe)
+git checkout gh-pages || git checkout --orphan gh-pages
+git rm -rf .
+
+# Copiar Stack-Tower-2D a la subruta correcta
+mkdir stack-tower
+cp -r Stack-Tower-2D/* stack-tower/
+
+# Copiar tienda-react a la subruta correcta
+mkdir tienda
+cp -r tienda-react/build/* tienda/
+
+# Commit y push a gh-pages
+git add .
+git commit -m "Actualización de GitHub Pages"
+git push origin gh-pages -f
+git checkout main #Volvemos a la rama principal (main)
